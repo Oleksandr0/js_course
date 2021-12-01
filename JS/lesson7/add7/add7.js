0ba3e70563de94060c1d510e58bb8cd1b0242ce9
Oleksandr0 <iermolchev80@gmail.com>
2021-12-01 19:42:40 +0200

    complete lesson7

--- JS/lesson7/add7/add7.js
+++ JS/lesson7/add7/add7.js
@@ -51,6 +51,22 @@ console.log(object2)
 // -назву атрибуту
 // -опис дії атрибуту
 // інформацію брати з htmlbook.ru
+
+// function Tag(titleOfTag,action,attrs) {
+// 	this.titleoftag = titleOfTag;
+// 	this.action = action;
+// 	this.attrs = attrs;
+// }
+// let title = "<a>";
+// let action = "Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок. В зависимости от присутствия атрибутов name или href тег <a> устанавливает ссылку или якорь. Якорем называется закладка внутри страницы, которую можно указать в качестве цели ссылки. При использовании ссылки, которая указывает на якорь, происходит переход к закладке внутри веб-страницы.";
+// let atribytes = [
+// 	{titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+// 	{titleOfAttr: "coords", actionOfAttr: 'Устанавливает координаты активной области.'},
+// 	{titleOfAttr: 'download', actionOfAttr: 'Предлагает скачать указанный по ссылке файл.'},
+// ];
+// let a = new Tag(title,action,atribytes);
+// console.log(a);
+
 //
 // Таким чином описати теги
 // -a
