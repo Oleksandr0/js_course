2d39c1ce34b2595b392638b5995477f232055da2
Oleksandr0 <iermolchev80@gmail.com>
2021-12-08 00:12:27 +0200

    complete lesson10

--- JS/Lesson10/add10/add10.js
+++ JS/Lesson10/add10/add10.js
@@ -9,19 +9,19 @@
 
 //
 // -- взять массив пользователей
-// let usersWithAddress = [
-//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-// ];
+let usersWithAddress = [
+    {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+    {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+    {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+    {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+    {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+    {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+    {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+    {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+    {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+];
 // - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
@@ -29,92 +29,90 @@
 // Данные выводить в документ
 // //
 // //
-// function array(userList) {
-//     for (const user of userList) {
-//         let userWrap = document.createElement('div');
-//         document.body.append(userWrap);
-//         for (const userkey in user) {
-//             if (typeof user[userkey] !== 'object') {
-//                 let element = document.createElement('div');
-//                 element.innerText = `${userkey}: ${user[userkey]}`;
-//                 userWrap.appendChild(element);
-//             } else {
-//                 let wrapper = document.createElement('div');
-//                 for (const itemkey in user[userkey]) {
-//                     if (typeof user[userkey][itemkey] !== 'object') {
-//                         let item = document.createElement('div');
-//                         item.innerText = `${itemkey}: ${user[userkey][itemkey]}`;
-//                         wrapper.appendChild(item);
-//                         userWrap.appendChild(wrapper);
-//                     }
-//                 }
-//             }
-//         }
-//         let hr = document.createElement('hr');
-//         userWrap.appendChild(hr);
-//     }
-//
-// }
-//
-//
-//
-// let f1 = document.forms.form;
-// let statusFilter = (array) => {
-//     let result;
-//     if (f1.status.checked) {
-//         result = array.filter(user => user.status === false);
-//     } else {
-//         result = usersWithAddress;
-//     }
-//     return result;
-// }
-//
-// let ageFilter = (array) => {
-//     let ageUser;
-//     if (f1.age.checked) {
-//         ageUser = array.filter(user => (user.age >= 29))
-//     } else {
-//         result = usersWithAddress;
-//     }
-//     return ageUser;
-// }
-//
-//
-// let cityFilter = (array) => {
-//     let cityKyiv;
-//     if (f1.city.checked) {
-//         cityKyiv = array.filter(user => (user.address.city === 'Kyiv'))
-//     } else {
-//         result = usersWithAddress;
-//     }
-//     return cityKyiv;
-// }
-//
-// let button = document.getElementById('result');
-// button.onclick = function () {
-//     if (f1.status.checked && !f1.age.checked && !f1.city.checked) {
-//         let statusResult = statusFilter(usersWithAddress);
-//         array(statusResult);
-//     } else if (!f1.status.checked && f1.age.checked && !f1.city.checked) {
-//         let ageResult = ageFilter(usersWithAddress);
-//         array(ageResult);
-//     } else if (!f1.status.checked && !f1.age.checked && f1.city.checked) {
-//         let cityResult = cityFilter(usersWithAddress);
-//         array(cityResult);
-//     } else if (f1.status.checked && f1.age.checked && f1.city.checked) {
-//         let allfilters = statusFilter(ageFilter(cityFilter(usersWithAddress)));
-//         array(allfilters);
-//     } else if (!f1.status.checked && f1.age.checked && f1.city.checked) {
-//         let ageCity = ageFilter(cityFilter(usersWithAddress));
-//         array(ageCity);
-//     } else if (f1.status.checked && f1.age.checked && !f1.city.checked) {
-//         let statusAge = statusFilter(ageFilter(usersWithAddress));
-//         array(statusAge);
-//     } else if (f1.status.checked && !f1.age.checked && f1.city.checked){
-//         let statusCity = statusFilter(cityFilter(usersWithAddress));
-//         array(statusCity);
-//     }
-// }
+function array(userList) {
+    for (const user of userList) {
+        let userWrap = document.createElement('div');
+        document.body.append(userWrap);
+        for (const userkey in user) {
+            if (typeof user[userkey] !== 'object') {
+                let element = document.createElement('div');
+                element.innerText = `${userkey}: ${user[userkey]}`;
+                userWrap.appendChild(element);
+            } else {
+                let wrapper = document.createElement('div');
+                for (const itemkey in user[userkey]) {
+                    if (typeof user[userkey][itemkey] !== 'object') {
+                        let item = document.createElement('div');
+                        item.innerText = `${itemkey}: ${user[userkey][itemkey]}`;
+                        wrapper.appendChild(item);
+                        userWrap.appendChild(wrapper);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+
+let f1 = document.forms.form;
+let statusFilter = function (array) {
+    let result;
+    if (f1.status.checked) {
+        result = array.filter(user => user.status === false);
+    } else {
+        result = usersWithAddress;
+    }
+    return result;
+}
+
+let ageFilter = function (array) {
+    let ageUser;
+    if (f1.age.checked) {
+        ageUser = array.filter(user => (user.age >= 29))
+    } else {
+        result = usersWithAddress;
+    }
+    return ageUser;
+}
+
+
+let cityFilter = function (array) {
+    let cityKyiv;
+    if (f1.city.checked) {
+        cityKyiv = array.filter(user => (user.address.city === 'Kyiv'))
+    } else {
+        result = usersWithAddress;
+    }
+    return cityKyiv;
+}
+
+let button = document.getElementById('result');
+button.onclick = function () {
+    if (f1.status.checked && !f1.age.checked && !f1.city.checked) {
+        let statusResult = statusFilter(usersWithAddress);
+        array(statusResult);
+    } else if (!f1.status.checked && f1.age.checked && !f1.city.checked) {
+        let ageResult = ageFilter(usersWithAddress);
+        array(ageResult);
+    } else if (!f1.status.checked && !f1.age.checked && f1.city.checked) {
+        let cityResult = cityFilter(usersWithAddress);
+        array(cityResult);
+    } else if (f1.status.checked && f1.age.checked && f1.city.checked) {
+        let allfilters = statusFilter(ageFilter(cityFilter(usersWithAddress)));
+        array(allfilters);
+    } else if (!f1.status.checked && f1.age.checked && f1.city.checked) {
+        let ageCity = ageFilter(cityFilter(usersWithAddress));
+        array(ageCity);
+    } else if (f1.status.checked && f1.age.checked && !f1.city.checked) {
+        let statusAge = statusFilter(ageFilter(usersWithAddress));
+        array(statusAge);
+    } else if (f1.status.checked && !f1.age.checked && f1.city.checked){
+        let statusCity = statusFilter(cityFilter(usersWithAddress));
+        array(statusCity);
+    }
+}
 
 //
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
